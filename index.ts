3e56b67f54ad8f02ad449032aa43b53b4d480a70
ItzCrazyKns <kushagra20103@gmail.com>
2024-03-15 12:31:22 +0530

    feat(model): Adjust temperature

--- index.ts
+++ index.ts
@@ -16,6 +16,7 @@ config()
 const main = async () => {
     const model = new ChatOpenAI({
         modelName: "gpt-3.5-turbo",
+        temperature: 0.7
     })
 
     const promptTemplate = new ChatPromptTemplate({
